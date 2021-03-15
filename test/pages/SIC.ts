ceef3236fe3f078090da91a1cb336813b4e5a64a
Mus Radi <radi_mus2005@yahoo.fr>
2021-03-15 10:40:00 -0400

    cbs issue

--- test/pages/SIC.ts
+++ test/pages/SIC.ts
@@ -10,7 +10,9 @@ const locators = {
   'RP simulator response': 'https://rp1.tbstest.catslab.ca/RPSimulator/response',
   'RP simulator 2 response': 'https://rp2.tbstest.catslab.ca/RPSimulator/response',
   'QA chooser': 'https://qa-auth.id.alpha.canada.ca/oxauth/en/select.htm',
-  'OpenID Connect client': 'https://dev3-auth.id.alpha.canada.ca/oxauth/restv1/authorize?response_type=code&client_id=032fca4e-cf81-4366-994e-b40f36c44ce7&scope=openid&redirect_uri=https%3A%2F%2Fwww.canada.ca%2Fen.html&state=123&nonce=456',
+//  'OpenID Connect client': 'https://dev3-auth.id.alpha.canada.ca/oxauth/restv1/authorize?response_type=code&client_id=032fca4e-cf81-4366-994e-b40f36c44ce7&scope=openid&redirect_uri=https%3A%2F%2Fwww.canada.ca%2Fen.html&state=123&nonce=456',
+  'OpenID Connect client': 'https://qa-auth.id.alpha.canada.ca/oxauth/restv1/authorize?response_type=code&client_id=03af1a23-e2c6-4a96-80e4-e00377b56ab4&scope=openid&redirect_uri=https%3A%2F%2Fwww.canada.ca%2Fen.html&state=123&nonce=456',
+
   // end of pages
   'Level 2': '//*[@id="cn-left-col-inner"]/section/div/nav/form/section[1]/ul/li[2]/input',
   'Force Authn': '//*[@id="cn-left-col-inner"]/section/div/nav/form/section[2]/ul/li[1]/input',
@@ -18,7 +20,7 @@ const locators = {
   CBS: 'CBS-UAT',
   SIC: process.env.CSP_HOST || 'Sign In Canada - QA',
   Continue: '//*[@id="continue"]',
-  CBSTest1: '//*[@id="SK_TEST1"]',
+  CBSTest1: '//a[@id="SK_TEST1"]',
   'Sign up with GCKey': '//*[@id="loginForm:loginButton3"]',
   'Sign Up': {
     xpath: '/html/body/div[1]/main/div[2]/div/div/div/div/div/div[3]/div/p[3]/a',
